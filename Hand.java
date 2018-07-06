b23880feaacd29045f7e7e07c4d68e823b8f66a3
Mergarcia <37250614+Mergarcia@users.noreply.github.com>
2018-07-06 13:57:04 -0700

    Add files via upload

---
+++ Hand.java
@@ -0,0 +1,22 @@
+/*************************************
+ Name: Eric Ybarra, Mercedes Garcia
+       Thomas Krause, William Barajas
+ Module 3              
+ CST 338 - "Deck of Cards"
+    This program simulates a card game
+    with the use of three classes: Card,
+    Hand, & Deck. We use inheritance to 
+    incorporate all these classes into
+    one program.
+ 
+ *************************************/
+public class Hand
+{
+
+   public static void main(String[] args)
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+}
