f14fcf8bf9a6f105ade4d3e6dd0570b573be129c
eybarra9 <erybarra@csumb.edu>
2018-07-06 22:32:44 -0700

    Started the Hand class
    Started the hand class but it still needs a few things such as the Card inspectCard(int k) method.

--- src/Hand.java
+++ src/Hand.java
@@ -3,9 +3,59 @@
  * <p>
  * CST 338 - M3 Deck of Cards
  *
- * @author Your Name Here
+ * @author Eric Ybarra, Willan Barajas
  * @version 1.0
  */
-public class Hand {
-
+public class Hand 
+{
+   // Constants
+   public static final int MAX_CARDS = 50;
+   
+   // Private Data Members
+   private Card[] myCards;
+   private int numCards;
+   
+   // Default Constructor
+   public Hand()
+   {
+      resetHand();
+   }
+   
+   // Resets the hand 
+   public void resetHand()
+   {
+      myCards = new Card[MAX_CARDS];
+      numCards = 0;
+   }
+   
+   //Adds a card to the next available position in the 
+   // myCards array
+   public boolean takeCard(Card card)
+   {
+      if (numCards >= MAX_CARDS)
+      {
+         return false; 
+      }
+      
+   }
+   
+   // Returns and removes the card in the top occupied
+   // position of the array.
+   public Card playCard()
+   {
+      if(numCards <= 0)
+      {
+         return new Card; 
+      }
+      Card card = new Card(my cards[--numCards]);
+      myCards[numCards] = null;
+      return card;
+   }
+   
+   // Returns the current number of cards
+   public int getNumCards()
+   {
+      return numCards;
+   }
+   
 }
